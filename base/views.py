cd03c3f9621f43b34634c82aeacaff90c844e5f3
gabrielFzDev <gabrielFz2001@hotmail.com>
2023-06-12 16:25:39 -0300

    Page Home 50% complete

--- base/views.py
+++ base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from .models import Activity
+from .models import Activity,CategoryAction,Action,Projects
+from django.db.models import Q
 # Create your views here.
 def homeView(request):
+    projects = Projects.objects.all()
     activity = Activity.objects.all()
-    context = {'activity':activity}
+    category = CategoryAction.objects.all()
+    if request.GET.get('queryC') != None:
+        qc = request.GET.get('queryC') 
+        act = Action.objects.filter(category_id=qc)
+    else:
+        act = None
+    
+    context = {'activity':activity,'category':category,'acts':act,'projects':projects}
     return render(request,'base/home.html',context)
